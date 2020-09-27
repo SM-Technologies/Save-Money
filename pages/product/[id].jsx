2f8c77247418527ec98da1786ac3793235aa8c56
andrés ricaño cesar <aricanomx@gmail.com>
2020-09-27 17:03:20 -0500

    solve problems with the integration of dinamyc path

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 const ProductItem = () => {
-  const {
-    query: { id },
-  } = useRouter();
-  return (
-    <div>
-      <h1>hello World {id}</h1>
-    </div>
-  );
+  // const {
+  //   query: { id },
+  // } = useRouter();
+  // return (
+  //   <div>
+  //     <h1>hello World {id}</h1>
+  //   </div>
+  // );
 };
 
 export default ProductItem;
