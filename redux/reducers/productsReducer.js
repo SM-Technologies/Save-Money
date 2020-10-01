72718b96c279a0298fcaac03316a28686ff73eab
brahianf <brahianf@unicauca.edu.co>
2020-10-01 12:56:25 -0500

    Redux, reducer and actions Products

---
+++ redux/reducers/productsReducer.js
@@ -0,0 +1,43 @@
+const INITIAL_STATE = {
+  error: null,
+  status: 'idle',
+  offerProducts: '',
+  dataProducts: '',
+  productById: '',
+  productByName: '',
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'DATA_OFF_PRODUCTS':
+      return {
+        ...state,
+        offerProducts: action.payload,
+        loading: false,
+        error: null,
+      };
+    case 'DATA_PRODUCTS':
+      return {
+        ...state,
+        dataProducts: action.payload,
+        loading: false,
+        error: null,
+      };
+    case 'PRODUCT_BY_ID':
+      return {
+        ...state,
+        productById: action.payload,
+        loading: false,
+        error: null,
+      };
+    case 'PRODUCT_BY_NAME':
+      return {
+        ...state,
+        productByName: action.payload,
+        loading: false,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
