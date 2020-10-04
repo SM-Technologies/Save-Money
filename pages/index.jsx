69bd8a9673cc0f73beb90a06c46b869ef31989e7
brahianf <brahianf@unicauca.edu.co>
2020-10-04 12:16:42 -0500

    Conection 2 Home and Store

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
-import useSWR from 'swr';
-
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from '../redux/reducers/rootReducer';
 
 import Layout from '../components/layout/Layout';
 import ContainerProducts from '../components/productsContainer/HomeContainer';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+import store from '../redux/store';
 
 export default function Home() {
   return (
@@ -26,12 +21,3 @@ export default function Home() {
     </>
   );
 }
-
-// function Allproducts() {
-//   const { getProduct, error } = useSWR('http://35.198.26.40/api-docs', fetcher);
-//   if (error) return <div>failed to load</div>;
-//   if (!getProduct) return <div>loading...</div>;
-//   console.log({ getProduct: id });
-//   // return <div>hello {data.id}!</div>;
-//   return {};
-// }
