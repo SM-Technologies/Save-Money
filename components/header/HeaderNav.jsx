f58da5a34847f3d1ca79549a5a0b764a9b1fb8eb
andrés ricaño cesar <aricanomx@gmail.com>
2020-10-04 23:42:15 -0500

    add new region on develop branch

--- components/header/HeaderNav.jsx
+++ components/header/HeaderNav.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 import Category from '../../src/assets/icons/cuadrado.svg';
 import Store from '../../src/assets/icons/compras.svg';
-import Country from '../../src/assets/icons/Brands/mexico.svg';
+import Country from '../../src/assets/images/usa.png';
 import Arrow from '../../src/assets/icons/arrow-down.svg';
 
 export default function NavHeader() {
